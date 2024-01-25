8c8d5eed4fca1e34a44abb502c629c8eb2451e49
fionamei131@gmail.com <fionamei131@gmail.com>
2024-01-24 21:51:09 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -34,6 +43,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -48,6 +60,9 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
+
 class Sub: 
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -58,19 +73,45 @@ class Sub:
             return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly, poly.evaluate(-1))
 
-print(Mul(Add(3, 4), Add(5, Int(5))))
-print(Mul(Div(Add(3, 5), Mul(6, Int(3))), 3))
-print(Add(Int(3), Mul(X(), 2)))
-print(Mul(Div(5, X()), Add(Int(2), X())))
-print(Div(Sub(Int(8), 3), Mul(X(), 2)))
-print(Sub(Mul(Int(4), X()), Div(X(), 2)))
-print(Mul(Add(Int(1), X()), Div(Int(3), 2)))
-print(Div(Mul(Int(2), X()), Add(X(), 4)))
-print(Add(Sub(X(), Int(1)), Mul(X(), 3)))
-print(Sub(Div(X(), Int(2)), Add(X(), Int(1))))
-print(Mul(Div(Int(6), X()), Add(X(), 2)))
-print(Div(Sub(Mul(Int(5), X()), 3), Add(X(), 1)))
+poly1 = Mul(Add(Int(3), Int(4)), Add(Int(5), Int(5)))
+print(f"{poly1} = {poly1.evaluate(0)}")
+
+poly2 = Mul(Div(Add(Int(3), Int(5)), Mul(Int(6), Int(3))), Int(3))
+print(f"{poly2} = {poly2.evaluate(0)}")
+
+poly3 = Add(Int(3), Mul(X(), Int(2)))
+print(f"{poly3} = {poly3.evaluate(5)}")
+
+poly4 = Mul(Div(Int(5), X()), Add(Int(2), X()))
+print(f"{poly4} = {poly4.evaluate(2)}")
+
+poly5 = Div(Sub(Int(8), Int(3)), Mul(X(), Int(2)))
+print(f"{poly5} = {poly5.evaluate(4)}")
+
+poly6 = Sub(Mul(Int(4), X()), Div(X(), Int(2)))
+print(f"{poly6} = {poly6.evaluate(3)}")
+
+poly7 = Mul(Add(Int(1), X()), Div(Int(3), Int(2)))
+print(f"{poly7} = {poly7.evaluate(1)}")
+
+poly8 = Div(Mul(Int(2), X()), Add(X(), Int(4)))
+print(f"{poly8} = {poly8.evaluate(3)}")
+
+poly9 = Add(Sub(X(), Int(1)), Mul(X(), Int(3)))
+print(f"{poly9} = {poly9.evaluate(2)}")
+
+poly10 = Sub(Div(X(), Int(2)), Add(X(), Int(1)))
+print(f"{poly10} = {poly10.evaluate(6)}")
+
+poly11 = Mul(Div(Int(6), X()), Add(X(), Int(2)))
+print(f"{poly11} = {poly11.evaluate(3)}")
+
+poly12 = Div(Sub(Mul(Int(5), X()), Int(3)), Add(X(), Int(1)))
+print(f"{poly12} = {poly12.evaluate(4)}")
